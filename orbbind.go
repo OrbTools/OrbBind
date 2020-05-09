9e9b366cd2d10a75aeb422f2a4dca6700d8b879b
Mini <Mini@zbots.org>
2020-05-09 00:29:30 -0400

    remove now obsolete workaround

--- orbbind.go
+++ orbbind.go
@@ -24,32 +24,9 @@ func main() {
 	pages["side"] = sidepage.NewSidePage(window, omap)
 	tabs := widget.NewTabContainer(pages["main"].Create(), pages["side"].Create())
 	tabs.Resize(fyne.NewSize(640, 480))
-	dmenu := widget.NewHBox(widget.NewButton("Save", func() {
-		dialog.ShowFileSave(func(writer fyne.FileWriteCloser, err error) {
-			if err != nil {
-				dialog.ShowError(err, window)
-				return
-			}
-			if writer != nil {
-				orbweaver.SaveIntoKeymap(omap, writer)
-			}
-		}, window)
-	}), widget.NewButton("Load", func() {
-		dialog.ShowFileOpen(func(reader fyne.FileReadCloser, err error) {
-			if err != nil {
-				dialog.ShowError(err, window)
-				return
-			}
-			if reader != nil {
-				omap = orbweaver.LoadFile(reader)
-				pages["main"].SetBindings(omap)
-				pages["side"].SetBindings(omap)
-			}
-		}, window)
-	}))
-	main := widget.NewVBox(dmenu, tabs)
+	main := tabs
 	window.Resize(fyne.NewSize(640, 500))
-	/*mainMenu := fyne.NewMainMenu(fyne.NewMenu("File", fyne.NewMenuItem("Save", func() {
+	mainMenu := fyne.NewMainMenu(fyne.NewMenu("File", fyne.NewMenuItem("Save", func() {
 		dialog.ShowFileSave(func(writer fyne.FileWriteCloser, err error) {
 			if err != nil {
 				dialog.ShowError(err, window)
@@ -69,7 +46,7 @@ func main() {
 		}, window)
 	})))
 	window.SetMainMenu(mainMenu)
-	*/
+
 	window.SetContent(main)
 	window.ShowAndRun()
 }
