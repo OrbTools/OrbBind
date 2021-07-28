3feeb1c8f1d7ecb4156e8f27d35eb3b7acb53c48
Minizbot2012 <bradleyfwin@gmail.com>
2021-07-28 02:52:32 -0400

    fixes for new OrbCommon

--- ui/mui/generator.go
+++ ui/mui/generator.go
@@ -10,18 +10,17 @@ import (
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/OrbTools/OrbBind/ui/bind"
-	"github.com/OrbTools/OrbCommon/gui"
+	"github.com/OrbTools/OrbCommon/devices"
 )
 
 //Generate creates a GUI from definition
-func Generate(ui *gui.GUI, window fyne.Window, KBS reflect.Value) (*container.AppTabs, func(reflect.Value), func() reflect.Value) {
+func Generate(ui *devices.DeviceDef, window fyne.Window, KBS reflect.Value) (*container.AppTabs, func(reflect.Value), func() reflect.Value) {
 	keybind := KBS
 	tui := container.NewAppTabs()
-	for _, page := range ui.Pages[:] {
+	for _, page := range ui.GuiPages {
 		var cont *fyne.Container
-		pg := page
 		switch page.Type {
-		case gui.PGrid:
+		case "PGrid":
 			{
 				cont = container.New(layout.NewGridLayout(int(math.Ceil(math.Sqrt(float64(len(page.Keys)))))))
 			}
@@ -33,13 +32,13 @@ func Generate(ui *gui.GUI, window fyne.Window, KBS reflect.Value) (*container.Ap
 		for _, key := range page.Keys[:] {
 			ky := key
 			btn := widget.NewButton(ky.KeyName, func() {
-				bp := bind.NewBindPage(ky.KeyID, window, uint16(keybind.Elem().FieldByName(pg.Hive).Index(ky.KeyID).Uint()))
+				bp := bind.NewBindPage(ky.KeyID, window, uint16(keybind.Elem().FieldByName("Keymap").Index(ky.KeyID).Uint()))
 				cont := bp.Create(ky.KeyName)
 				ok := func(ok bool) {
 					kb := keybind.Elem()
 					if ok {
-						field := kb.FieldByName(pg.Hive)
-						if field.Kind() == reflect.Array {
+						field := kb.FieldByName("Keymap")
+						if field.Kind() == reflect.Array || field.Kind() == reflect.Slice {
 							field.Index(ky.KeyID).SetUint(uint64(bp.Bind.Bound))
 						}
 					}
